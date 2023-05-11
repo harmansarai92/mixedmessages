c8250f7271e6cf3ea13eb6d5ea3c0c9dce51951d
HSarai <harmansarai92@gmail.com>
2023-05-11 18:37:50 +0100

    Added arrays

---
+++ mixed.js
@@ -0,0 +1,7 @@
+function getRandom(num) {
+
+}
+
+let baseMeal = ['bacon', 'oats', 'tuna', 'toast'];
+let mealAddition = ['strawberry', 'ice cream', 'tomato', 'potatoes'];
+let drink = ['orange juice', 'cola', 'water', 'tea'];
