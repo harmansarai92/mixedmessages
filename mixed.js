891b5f479bdef79d1679d0cf58112d235092eba0
HSarai <harmansarai92@gmail.com>
2023-05-11 20:39:33 +0100

    Added comments

--- mixed.js
+++ mixed.js
@@ -2,14 +2,14 @@ const randomMeal = {
     baseMeal: ['bacon', 'oats', 'tuna', 'toast'],
     sideMeal: ['strawberry', 'ice cream', 'tomato', 'potatoes'],
     drink: ['orange juice', 'cola', 'water', 'tea'],
-    getRandomBaseMeal: function() {
+    getRandomBaseMeal: function() { //This is to get random base meal.
         randomBase = Math.floor(Math.random() * this.baseMeal.length);
         return this.baseMeal[randomBase];
-    },
+    }, //This is to get random side meal.
     getRandomSideMeal: function() {
         randomSide = Math.floor(Math.random() * this.sideMeal.length);
         return this.sideMeal[randomSide];
-    },
+    }, //This is to get random drink.
     getRandomDrink: function() {
         randomDrink = Math.floor(Math.random() * this.drink.length);
         return this.drink[randomDrink];
