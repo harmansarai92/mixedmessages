73ff0115a57e9e1581a0a03c745496a4f3a83e10
HSarai <harmansarai92@gmail.com>
2023-05-11 18:44:18 +0100

    Added random function

--- mixed.js
+++ mixed.js
@@ -1,7 +1,8 @@
 function getRandom(num) {
-
+    return Math.floor(Math.random() * 3);
 }
 
 let baseMeal = ['bacon', 'oats', 'tuna', 'toast'];
 let mealAddition = ['strawberry', 'ice cream', 'tomato', 'potatoes'];
 let drink = ['orange juice', 'cola', 'water', 'tea'];
+
