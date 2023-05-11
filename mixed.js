d92baaa2c1fb93eb957c80bc663e5922b2133df4
HSarai <harmansarai92@gmail.com>
2023-05-11 20:01:00 +0100

    Added console.log

--- mixed.js
+++ mixed.js
@@ -1,11 +1,20 @@
-function getRandom() {
-    let randBaseMeal = Math.floor(Math.random() * baseMeal.length - 1);
-    let randMealAddition = Math.floor(Math.random() * mealAddition);
-    let randomDrink = Math.floor(Math.random() * 4);
-}
-
 let randomMeal = {
     baseMeal: ['bacon', 'oats', 'tuna', 'toast'],
     sideMeal: ['strawberry', 'ice cream', 'tomato', 'potatoes'],
-    drink: ['orange juice', 'cola', 'water', 'tea']
+    drink: ['orange juice', 'cola', 'water', 'tea'],
+    getRandomBaseMeal: function() {
+        randomBase = Math.floor(Math.random() * this.baseMeal.length -1);
+        return baseMeal[randomBase];
+    },
+    getRandomSideMeal: function() {
+        randomSide = Math.floor(Math.random() * this.sideMeal.length - 1);
+        return sideMeal[randomSide];
+    },
+    getRandomDrink: function() {
+        randomDrink = Math.floor(Math.random() * this.drink.length - 1);
+        return drink[randomDrink];
+    }
 }
+
+
+console.log('I am going to have a ' + randomMeal.getRandomBaseMeal() + ' with a side of ' + randomMeal.getRandomSideMeal() + ' and a nice cold glass of ' + randomMeal.getRandomDrink());
